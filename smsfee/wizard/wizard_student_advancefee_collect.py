ed3c71b16a2800ba557651bbf642c102c75db916
Ghulam Masood <ghulam.masood@gmail.com>
2017-02-14 21:34:27 +0500

    Changes in Wizard for receving advanced fee payment

--- smsfee/wizard/wizard_student_advancefee_collect.py
+++ smsfee/wizard/wizard_student_advancefee_collect.py
@@ -19,9 +19,23 @@ class class_student_advancefee_collect(osv.osv_memory):
     _defaults = {
                  'student_id':_get_student,
                  }
+
+    def check_classwise_fee_structure(self, cr, uid, thisform):
+        
+        student_id = self.pool.get('sms.student').search(cr, uid, [('id','=',thisform['student_id'][0])])
+        student_rec = self.pool.get('sms.student').browse(cr, uid, student_id)
+        for rec in student_rec:
+            class_feestruct_id = self.pool.get('smsfee.classes.fees').search(cr, uid, [('academic_cal_id','=',rec.current_class.id),('fee_structure_id','=',rec.fee_type.id)])
+            print "-----",class_feestruct_id
+            class_feestruct_lines_id = self.pool.get('smsfee.classes.fees.lines').search(cr, uid, [('parent_fee_structure_id','=',class_feestruct_id),('fee_type','=',thisform['fee_types'][0])])
+            class_feestruct_lines_obj = self.pool.get('smsfee.classes.fees.lines').browse(cr, uid, class_feestruct_lines_id)
+            print "++++++",class_feestruct_lines_obj
+        print alpha
+        return True
     
     def collect_advancefee_student(self, cr, uid, ids, data):
         thisform = self.read(cr, uid, ids)[0]
+        calling_check1 = self.check_classwise_fee_structure(cr, uid, thisform)
         report = 'smsfee_print_three_student_per_page'
         datas = {
              'ids': [],
