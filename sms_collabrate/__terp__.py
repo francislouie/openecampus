43c53d0fbe276e13dab6d77324b0b3843c0e21c7
Shahidd <shahidislam@imsciences.edu.pk>
2017-03-20 23:58:42 +0500

    new module sms_collabrate is added

---
+++ sms_collabrate/__terp__.py
@@ -0,0 +1,15 @@
+{
+        "name" : "Server as connector betwenn PHP Portal app and SMS ERP system",
+        "version" : "0.1",
+        "author" : "Shahid Islam",
+        "website" : "http://www.cyme.com.pk/",
+        "category" : "Campus Management System ",
+        "description": """
+        This Module will provide connection interface for data transfer between CMS and NSD.
+  """,
+        "depends"    : ['base','sms'],
+        "init_xml"   : [],
+        "demo_xml"   : [],
+        "update_xml" : [],
+        "installable": True
+}
