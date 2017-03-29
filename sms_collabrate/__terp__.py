fcf06b43520e38ef521ba958bfbcd4f9f75f0139
Shahidd <shahidislam@imsciences.edu.pk>
2017-03-29 21:46:08 +0500

    secuirty files removed from sms

--- sms_collabrate/__terp__.py
+++ sms_collabrate/__terp__.py
@@ -5,7 +5,7 @@
         "website" : "http://www.cyme.com.pk/",
         "category" : "Campus Management System ",
         "description": """
-        This Module will provide connection interface for data transfer between CMS and NSD.
+        This Module will provide connection interface for data transfer between .
   """,
         "depends"    : ['base','sms'],
         "init_xml"   : [],
