456afbb622180bb8b4d5232b9d71616ab8dbf656
Ghulam Masood <ghulam.masood@gmail.com>
2017-04-10 23:35:48 +0500

    Added Two methods for students portal

--- sms_collabrate/sms_collabrate.py
+++ sms_collabrate/sms_collabrate.py
@@ -17,21 +17,88 @@ class sms_collabrator(osv.osv):
            
     def mast_auth(self, cr, uid, login, pwd):
         result = []
-        student_id = self.pool.get('sms.student').search(cr,uid,[('login_id','=',login),('state','=','Admitted')])
+        student_id = self.pool.get('sms.student').search(cr,uid,[('login_id','=',login), ('state','=','Admitted')])
         if student_id:
             obj = self.pool.get('sms.student').browse(cr, uid, student_id)
             my_dict = {
                         'registration_no':obj[0].registration_no,
                         'stdname':obj[0].name,
                         'fathername':obj[0].father_name,
-                        'current_class_id':obj[0].current_class.id,
-                        'current_class':obj[0].current_class.name,
+                        'class_id':obj[0].current_class.id,
+                        'class_name':obj[0].current_class.name,
                         'pic':obj[0].image,
                         'std_id':obj[0].id,
+                        'state':obj[0].state,
+                        'login_status':1
+                    }
+#            objjj = self.pool.get('sms.collabrator').getstudent_personal_info(cr, uid, student_id)
+#            print "------",objjj
+            objjj = self.pool.get('sms.collabrator').getstudent_subjects(cr, uid, student_id)
+            print "------", objjj
+            result.append(my_dict)
+        else:
+            my_dict = {
+                        'state':'Invalid Credentials',
+                        'login_status':0
+                    }
+            result.append(my_dict)
+        return result
+    
+    def getstudent_personal_info(self, cr, uid, student_id):
+        result = []
+        student_id = self.pool.get('sms.student').search(cr,uid,[('id','=', student_id), ('state','=','Admitted')])
+        if student_id:
+            obj = self.pool.get('sms.student').browse(cr, uid, student_id)
+            my_dict = {
+                        'registration_no':obj[0].registration_no,
+                        'stdname':obj[0].name,
+                        'fathername':obj[0].father_name,
+                        'class_id':obj[0].current_class.id,
+                        'class_name':obj[0].current_class.name,
+                        'pic':obj[0].image,
+                        'std_id':obj[0].id,
+                        'state':obj[0].state,
+                        'blood_group':obj[0].blood_grp,
+                        'gender':obj[0].gender,
+                        'date_of_birth':obj[0].birthday,
+                        'father_nic':obj[0].father_nic,
+                        'contact_no_1':obj[0].phone,
+                        'contact_no_2':obj[0].cell_no,
+                        'email':obj[0].email,
+                        'address':obj[0].cur_address,
+                        'city':obj[0].cur_city,
+                        'login_status':1
                     }
             result.append(my_dict)
         else:
-            return 'Invalid User name or Password'
+            my_dict = {
+                        'state':'Invalid Credentials',
+                        'login_status':0
+                    }
+            result.append(my_dict)
+        return result
+    
+    def getstudent_subjects(self, cr, uid, student_id):
+        result = []
+        student_subj_id = self.pool.get('sms.student.subject').search(cr,uid,[('student_id','=', student_id)])
+        if student_subj_id:
+            for subject in self.pool.get('sms.student.subject').browse(cr, uid, student_subj_id):
+                if subject.reference_practical_of:
+                    practical = subject.reference_practical_of.name
+                else:
+                    practical = None
+                my_dict = {
+                            'subject_name':subject.subject.name,
+                            'subject_status':subject.subject_status,
+                            'reference_practical_of':practical,
+                            'login_status':1
+                        }
+                result.append(my_dict)
+            else:
+                my_dict = {
+                            'login_status':0
+                        }
+                result.append(my_dict)
         return result
     
 sms_collabrator()
