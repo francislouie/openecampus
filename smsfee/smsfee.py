11dbe3c0bb2ae55b9fe441a88f49763d1326ce3c
cyme7 <bilal@cyme.com.pk>
2018-03-17 21:58:14 +0500

    cancel challans

--- smsfee/smsfee.py
+++ smsfee/smsfee.py
@@ -1887,20 +1887,44 @@ class smsfee_receiptbook(osv.osv):
                 sql = """SELECT receipt_date from smsfee_receiptbook where id= """+str(f.id)
                 cr.execute(sql)
                 rdate = cr.fetchone()[0]
-                date_convt = datetime.datetime.strptime(str(rdate) , '%Y-%m-%d')
+                date_convt = datetime.datetime.strptime(str(rdate) , '%Y-%m-%d') #date convertion
                 date_str = str(date_convt.strftime('%d-%b-%Y'))
                 result[f.id] = date_str
             else:
                 result[f.id] = '--'
         return result
-    
     def cancel_fee_bill(self, cr, uid, ids, context={}, arg=None, obj=None):
+        # sql=""" Select id from smsfee_feetypes where subtype='Late Fee'bilal """
         result = {}
         records =  self.browse(cr, uid, ids, context)
         for f in records:
             if f.cancel_late_fee:
-                if  True: #f.due_date>datetime.date.today():
-                    call = self.pool.get('smsfee.studentfee').insert_student_monthly_non_monthlyfee(self, cr, uid,f.student_id.id, f.class_id.id, f.student_fee_id.id,f.fee_month.id)
+                print "today", datetime.date.today(), "due date", f.due_date
+                date_convt = datetime.datetime.strptime(str(f.due_date), '%Y-%m-%d')
+                late_fee_id = self.pool.get('smsfee.feetypes').search(cr, uid, [('subtype', '=', 'Late Fee')])
+                print 'late fee',late_fee_id
+                print 'converted date ',date_convt,"datetime.datetime.now()",datetime.datetime.now()
+                if  date_convt<datetime.datetime.now(): #class id issue
+                    fee_dcit = {
+                        'student_id': f.student_id.id,
+                        'acad_cal_id': f.student_class_id.id,
+                        'fee_type': '',
+                        'date_fee_charged': datetime.date.today(),
+                        'due_month': 0,
+                        'fee_month': 0,
+                        'paid_amount': 0,
+                        'fee_amount': f.late_fee,
+                        'late_fee': 0,
+                        'discount': 0,
+                        'total_amount': 0,
+                        'reconcile': False,
+                        'state': 'fee_unpaid',
+                        'generic_fee_type': late_fee_id[0]
+                    }
+
+                    create_fee = self.pool.get('smsfee.studentfee').create(cr, uid, fee_dcit)
+
+                    # call = self.pool.get('smsfee.studentfee').insert_student_monthly_non_monthlyfee(self, cr, uid,f.student_id, f.student_class_id, late_fee_id,f.student_class_id )
             self.write(cr, uid, f.id, {'state':'Cancel','challan_cancel_by':uid})
         return result
     
@@ -1945,7 +1969,7 @@ class smsfee_receiptbook(osv.osv):
             print "we are canceling challans:",challan_ids
             if challan_ids:
                 for challan_id in challan_ids:
-                    note="this fee bill is cancel by system on creation of new fee bill on date",datetime.datetime.now()
+                    note="this fee bill is cancelled by system on creation of new fee bill on date",datetime.datetime.now()
                     self.pool.get('smsfee.receiptbook').write(cr ,uid ,challan_id, {'state':'Cancel',
                                                                                     'note_at_receive':note,
                                                                                     'cancel_date':datetime.datetime.now()
@@ -1982,7 +2006,9 @@ class smsfee_receiptbook(osv.osv):
                     'receipt_book_id': receipt_id,
                     'fee_amount':unpaidfee.fee_amount,
                     'late_fee':0,
-                    'total':unpaidfee.fee_amount}
+                    'total':unpaidfee.fee_amount,
+                    'due_date':due_date
+                    }
                     print "book line dic ",feelinesdict
                     self.pool.get('smsfee.receiptbook.lines').create(cr ,uid,feelinesdict)
         return True
@@ -1992,10 +2018,9 @@ class smsfee_receiptbook(osv.osv):
     _description = "This object store fee types"
     _inherit = ['mail.thread']
     _order = "id desc"
-    
     _columns = {
         'name': fields.char('Bill No', readonly =True,size=15), 
-        'counter': fields.char('Bill No.', readonly =True,size=15),    
+        'counter': fields.char('Bill No.', readonly =True,size=15),
         'challan_cat':fields.selection([('Academics','Academics'),('Transport','Transport'),('Hostel','Hostel'),('Stationary','Stationary'),('Portal','Portal')],'Fee Category',readonly=True),
         'session_id':fields.many2one('sms.academics.session', 'Session'),
         'fee_structure': fields.char(string = 'Fee Structure',size = 100),
--- smsfee/wizard/smsfee_wizard_defaulter_students.py
+++ smsfee/wizard/smsfee_wizard_defaulter_students.py
@@ -112,6 +112,8 @@ class fee_defaulters(osv.osv_memory):
         
         selected_fee_list=[]
         for f in self.browse(cr, uid, ids):
+            if not f.fee_type_list:
+               raise osv.except_osv(('Please Select Fee'), ('Defaulter Excel Report Fees to be included'))
             for s in f.fee_type_list:
                 selected_fee_list.append(s.id)     
         #classes = self.pool.get('sms.academiccalendar').browse(cr,uid,41)
@@ -137,13 +139,13 @@ class fee_defaulters(osv.osv_memory):
                     sheet1=book.add_sheet(str(class_ctr)+" "+str(this_class.class_id.name),cell_overwrite_ok=True)
                     title = this_class.class_id.name
                     class_ctr = class_ctr + 1
-                    _col = (sheet1.col(1)).width = 200 * 15
-                    _col = (sheet1.col(2)).width = 300 * 15
-                    _col = (sheet1.row(3)).height = 300 * 15
-                    _col = (sheet1.row(3)).height = 200 * 15
+                    _col = (sheet1.col(0)).width = 200 * 15
+                    _col = (sheet1.col(1)).width = 300 * 15
+                    _col = (sheet1.row(2)).height = 300 * 15
+                    _col = (sheet1.row(2)).height = 200 * 15
                      
-                    sheet1.write(3,1,'Rego NO',header_top)
-                    sheet1.write(3,2,'Name',header_top)
+                    sheet1.write(3,0,'Rego NO',header_top)
+                    sheet1.write(3,1,'Name',header_top)
                      
                     #Find all fee types of this class and arrange in one reow of excel #changes: added subtype in select query
                     sqlfees = """ SELECT smsfee_feetypes.id,smsfee_feetypes.name,  
@@ -158,7 +160,7 @@ class fee_defaulters(osv.osv_memory):
  
                     cr.execute(sqlfees)
                     feerec = cr.fetchall()
-                    col_fee = 3# this is the column no in sheet for fee header
+                    col_fee = 2# this is the column no in sheet for fee header
                     fee_ids_list = []
                     month_dict_ids={}
                     adm_dict_ids={}
@@ -181,17 +183,17 @@ class fee_defaulters(osv.osv_memory):
                         #testing git branch 2
                         if fee[0] ==2:
                             if fee[0]==2:
-                                month_ids_list = []  #chnages: result2 replaced wiht month_ids_list
+                                month_ids_list = []
                                 for this_month in months:
                                     if f.developer_mode:                                
                                         ft_name = str(fee[1])+"\n"+str(this_month[1])+"\nmonth_id:"+str(this_month[0])+"\nfee_id:"+str(fee[0])
                                     else:
-                                        ft_name=str(fee[1])+"\n"+str(this_month[1])
+                                        ft_name=str(fee[1])+"\n"+str(this_month[1])[:3].upper()+str(this_month[1])[-5:]
                                     sheet1.write_merge(r1=0, c1=0, r2=2, c2=11)
                                     _col = (sheet1.row(col_fee)).height = 100 * 10
                                     #cell width for fee type other than months
                                     _col = (sheet1.col(col_fee)).width = 400 * 20
-                                    sheet1.write(0,3, title,header_top )
+                                    sheet1.write(0,2, title,header_top )
                                     sheet1.write(3,col_fee,ft_name,header_feetypes)
                                     month_ids_list.append(this_month[0])
                                     month_dict_ids[this_month[0]+fee[0]]=col_fee
@@ -203,7 +205,7 @@ class fee_defaulters(osv.osv_memory):
                             sheet1.write_merge(r1=0, c1=0, r2=2, c2=11)
                             _col = (sheet1.row(col_fee)).height = 100 * 10
                             _col = (sheet1.col(col_fee)).width = 200 * 20
-                            sheet1.write(0,3, title,header_feetypes )
+                            sheet1.write(0,2, title,header_feetypes )
                             if f.developer_mode:
                                 ft_name = str(fee[1])+"\nfee_id:"+str(fee[0])
                             else:
@@ -231,14 +233,14 @@ class fee_defaulters(osv.osv_memory):
                         _col = (sheet1.row(row)).height = 100 * 10
                         
                         if color:
-                            sheet1.write(row,1, this_student[1],student_grey_rows )
-                            sheet1.write(row,2, this_student[2],student_grey_rows )
+                            sheet1.write(row,0, this_student[1],student_grey_rows )
+                            sheet1.write(row,1, this_student[2],student_grey_rows )
                         else:
-                            sheet1.write(row,1, this_student[1],student_white_rows )
-                            sheet1.write(row,2, this_student[2],student_white_rows )
+                            sheet1.write(row,0, this_student[1],student_white_rows )
+                            sheet1.write(row,1, this_student[2],student_white_rows )
                        # print("student id",this_student[0],"\nmonth ids",month_ids_list ,"\nfee ids",fee_ids_list )
                         #now loop via the fees dictionaru for this student
-                        col_fee = 3
+                        col_fee = 2
                         col_month=25
                         col_adminiistrative=13
                          
@@ -246,7 +248,7 @@ class fee_defaulters(osv.osv_memory):
                            
                             if 2 in selected_fee_list:
                                 for month2 in month_ids_list:
-                                    sql5 = """select id,fee_month,fee_amount,generic_fee_type,state
+                                    sql5 = """select id,fee_month,fee_amount,generic_fee_type,state,receipt_no,date_fee_paid
                                                   from smsfee_studentfee where student_id =  """+str(this_student[0])+"""
                                               and generic_fee_type= """+str(fees2)+""" and fee_month="""+str(month2)
                                    
@@ -258,9 +260,10 @@ class fee_defaulters(osv.osv_memory):
                                         if found_fee[3]==2:
                                             if found_fee[4]=='fee_paid':
                                                 if f.developer_mode:
-                                                    label = 'Paid Amount:\n'+str(found_fee[2])+"\nmonth_id:"+str(month2)+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3]) 
+                                                    label = 'Paid Amount:\n'+str(found_fee[2])+"\nmonth_id:"+str(month2)+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3]) +'\n'+'Bill No:'+str(found_fee[5])+'Paid On :\n'+str(found_fee[6])
+                                                    print 'labellll',label
                                                 else:
-                                                    label='Paid Amount:\n'+str(found_fee[2]) 
+                                                    label='Paid Amount:\n'+str(found_fee[2])+'\nBill No:'+str(found_fee[5]) +'\nPaid On:'+str(found_fee[6])
                                                 sheet1.write(row,month_dict_ids[month2+fees2], label,paid_fee)
                                                 col_month = col_month + 1
                                                 col_fee=col_fee+1
@@ -280,7 +283,7 @@ class fee_defaulters(osv.osv_memory):
                                                 if f.developer_mode:
                                                     label = 'Paid Amount:\n'+str(found_fee[2])+"\nmonth_id:"+str(month2)+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])
                                                 else:
-                                                    label='Paid Amount:\n'+str(found_fee[2]) 
+                                                    label='Paid Amount:\n'+str(found_fee[2])+'\nBill No:'+str(found_fee[5]) +'\nPaid On:'+str(found_fee[6])
                                                 sheet1.write(row,annual_dict_ids[fees2], label,student_white_rows)
                                             else:
                                                 if f.developer_mode:
@@ -314,15 +317,15 @@ class fee_defaulters(osv.osv_memory):
                                         if found_fee[4]=='fee_paid':
                                             if f.developer_mode:
                                             
-                                                label = 'Paid Amount\n'+str(found_fee[2])+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3]) 
+                                                label = 'Paid Amount\n'+str(found_fee[2])+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3]) +'\n'+'Bill No:'+str(found_fee[5])+'Paid On :\n'+str(found_fee[6])
                                             else:
-                                                label='Paid Amount\n: '+str(found_fee[2])
+                                                label='Paid Amount\n: '+str(found_fee[2])+'\nBill No:'+str(found_fee[5]) +'\nPaid On:'+str(found_fee[6])
                                             sheet1.write(row,month_dict_ids[fees2], label,paid_fee)
                                             col_month = col_month + 1
                                             col_fee=col_fee+1
                                         else:
                                             if f.developer_mode:
-                                                label = 'Fee Amount:\n '+str(found_fee[2])+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3]) 
+                                                label = 'Fee Amount:\n '+str(found_fee[2])+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])
                                             else:
                                                 label='Fee Amount:\n '+str(found_fee[2])
                                             sheet1.write(row,month_dict_ids[fees2], label,unpaid_fee)
@@ -334,13 +337,13 @@ class fee_defaulters(osv.osv_memory):
                                     else:
                                         if found_fee[4]=='fee_paid':
                                             if f.developer_mode:
-                                                label = 'Paid Amount:\n'+str(found_fee[2])+"\nmonth_id:"+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])
+                                                label = 'Paid Amount:\n'+str(found_fee[2])+"\nmonth_id:"+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])+'\nBill No:'+str(found_fee[5]) +'\nPaid On:'+str(found_fee[6])
                                             else:
-                                                label='Paid Amount:\n '+str(found_fee[2])
+                                                label='Paid Amount:\n '+str(found_fee[2])+'\nBill No:'+str(found_fee[5]) +'\nPaid On :'+str(found_fee[6])
                                             sheet1.write(row,annual_dict_ids[fees2], label,student_white_rows)
                                         else:
                                             if f.developer_mode:
-                                                label = 'Fee Amount:\n '+str(found_fee[2])+"\nmonth_id:"+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])
+                                                label = 'Fee Amount:\n '+str(found_fee[2])+"\nmonth_id:"+"\nfee_id:"+str(fees2)+"\nfee_id_genric:"+str(found_fee[3])+'\nBill No:'+str(found_fee[5]) +'\nPaid On:'+str(found_fee[6])
                                             else:
                                                 label='Fee Amount:\n '+str(found_fee[2])
                                             sheet1.write(row,annual_dict_ids[fees2], label,student_white_rows)
