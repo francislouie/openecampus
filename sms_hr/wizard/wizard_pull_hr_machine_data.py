4f3e01d4a391332e5a718a7121b5a6ed6fc861de
Obaid <ubaid@cyme.com.pk>
2018-05-07 16:22:11 +0500

    multiple changes including transport challans

--- sms_hr/wizard/wizard_pull_hr_machine_data.py
+++ sms_hr/wizard/wizard_pull_hr_machine_data.py
@@ -164,32 +164,36 @@ class sms_pull_hr_machine_data(osv.osv_memory):
         branch_id = self.read(cr, uid, ids)[0]['branch_id']
         if not branch_id or branch_id == 0:
             raise osv.except_osv((),'No Branch ID Set, Cannot Proceed!')
-        # Check if there are inactive employees in the wizard 
-        inactive_id = self.read(cr, uid, ids)[0]['inactive_employees']
-        if inactive_id:
-            raise osv.except_osv((),'There are inactive employees in system, Cannot Proceed!')
-          
-          
-        # Check if there are employees with missing empleado ids in the wizard 
-        missing_empleado_id = self.read(cr, uid, ids)[0]['missing_empleado']
-        if missing_empleado_id:
-            raise osv.except_osv((),'Some employees have missing Empleado IDs, Cannot Proceed!')
-        # Check if there are employees with missing contracts in the wizard 
-        missing_contract_id = self.read(cr, uid, ids)[0]['missing_contract']
-        if missing_contract_id:
-            raise osv.except_osv((),'Some employees have no contracts, Cannot Proceed!')
-          
-          
-        # Check if there are employees with departments not assigned in the wizard 
-        department_not_set_id = self.read(cr, uid, ids)[0]['department_not_set']
-        if department_not_set_id:
-            raise osv.except_osv((),'Some employees have not been assigned proper Departments, Cannot Proceed!')
-          
-          
-        # Check if there are departments with no schedules in the wizard 
-        schedules_not_set_id = self.read(cr, uid, ids)[0]['schedules_not_set']
-        if schedules_not_set_id:
-            raise osv.except_osv((),'Some departments are without schedules, Cannot Proceed!')
+        
+        
+#         # Check if there are inactive employees in the wizard 
+#         inactive_id = self.read(cr, uid, ids)[0]['inactive_employees']
+#         if inactive_id:
+#             raise osv.except_osv((),'There are inactive employees in system, Cannot Proceed!')
+#            
+#            
+#         # Check if there are employees with missing empleado ids in the wizard 
+#         missing_empleado_id = self.read(cr, uid, ids)[0]['missing_empleado']
+#         if missing_empleado_id:
+#             raise osv.except_osv((),'Some employees have missing Empleado IDs, Cannot Proceed!')
+#         
+#         
+#         # Check if there are employees with missing contracts in the wizard 
+#         missing_contract_id = self.read(cr, uid, ids)[0]['missing_contract']
+#         if missing_contract_id:
+#             raise osv.except_osv((),'Some employees have no contracts, Cannot Proceed!')
+#            
+#            
+#         # Check if there are employees with departments not assigned in the wizard 
+#         department_not_set_id = self.read(cr, uid, ids)[0]['department_not_set']
+#         if department_not_set_id:
+#             raise osv.except_osv((),'Some employees have not been assigned proper Departments, Cannot Proceed!')
+#            
+#            
+#         # Check if there are departments with no schedules in the wizard 
+#         schedules_not_set_id = self.read(cr, uid, ids)[0]['schedules_not_set']
+#         if schedules_not_set_id:
+#             raise osv.except_osv((),'Some departments are without schedules, Cannot Proceed!')
           
 #          
 #         # Check if Month is selected in the wizard
@@ -232,7 +236,7 @@ class sms_pull_hr_machine_data(osv.osv_memory):
                 for date in current_month_dates:
                                         date_stamp = date.strftime('%Y%m%d')
                                         self.pool.get('hr.employee.attendance').create(cr, uid, {'employee_id': emp_id,'attendance_date': date_stamp, 
-                                            'sign_in': '','sign_out': '','final_status': 'Unknown','attendance_month': str(current_month_name)})
+                                            'sign_in': '','sign_out': '','final_status': 'unknown','attendance_month': str(current_month_name)})
         
         
             # create an entry against the current employee in the hr_monthly_attendance_calcualtion table
